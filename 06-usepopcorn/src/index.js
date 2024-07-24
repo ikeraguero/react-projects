2f88b00380971a049be0ea35596f173c724befff
ikeraguero <ikerpires407@gmail.com>
2024-07-24 19:38:32 -0300

    usePopcorn - Add Reusable StarRating Component

--- 06-usepopcorn/src/index.js
+++ 06-usepopcorn/src/index.js
@@ -1,9 +1,14 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import App from "./App";
+// import App from "./App";
+import StarRating from "./StarRating";
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <App />
+    <StarRating
+      color="#fcc419"
+      size={42}
+      messages={["Terrible", "Bad", "Ok", "Good", "Awesome"]}
+    />
   </React.StrictMode>
 );
