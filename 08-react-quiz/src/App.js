5588d9b3cb6e886c91a18c5cb96f5d8e289c9051
ikeraguero <ikerpires407@gmail.com>
2024-09-07 01:21:32 -0300

    Add Quiz Component

--- 08-react-quiz/src/App.js
+++ 08-react-quiz/src/App.js
@@ -1,6 +1,16 @@
 import DateCounter from "./DateCounter.js";
+import Header from "./Header.js";
+import Main from "./Main.js";
 import "./index.css";
 
 export default function App() {
-  return <DateCounter />;
+  return (
+    <div className="app">
+      <Header />
+      <Main>
+        <p>1/15</p>
+        <p>15 questions</p>
+      </Main>
+    </div>
+  );
 }
