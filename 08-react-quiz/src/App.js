e4a60084d25653f6dc5f97572a9f118fc189ea6a
ikeraguero <ikerpires407@gmail.com>
2024-09-10 18:41:06 -0300

    Add Quesstion Fetching to da to React Quiz

--- 08-react-quiz/src/App.js
+++ 08-react-quiz/src/App.js
@@ -1,16 +1,39 @@
+import { useEffect, useReducer } from "react";
 import DateCounter from "./DateCounter.js";
 import Header from "./Header.js";
 import Main from "./Main.js";
+import Loader from "./Loader.js";
 import "./index.css";
 
 export default function App() {
+  const initalState = {
+    status: "loading",
+    questions: [],
+  };
+
+  function reducer(state, action) {
+    switch (action.type) {
+      case "dataReceived":
+        return { status: "ready", questions: action.payload };
+      default:
+        return state;
+    }
+  }
+
+  const [state, dispatch] = useReducer(reducer, initalState);
+  useEffect(function () {
+    async function getData() {
+      const res = await fetch("http://localhost:9000/questions");
+      const data = await res.json();
+      dispatch({ type: "dataReceived", payload: data });
+    }
+    getData();
+  }, []);
+
   return (
     <div className="app">
       <Header />
-      <Main>
-        <p>1/15</p>
-        <p>15 questions</p>
-      </Main>
+      <Main>{state.status === "loading" && <Loader />}</Main>
     </div>
   );
 }
