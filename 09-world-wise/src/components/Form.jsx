f881c1bb58c86f675a3fd91d85f0ac009ec268a9
ikeraguero <ikerpires407@gmail.com>
2024-10-06 00:49:02 -0300

    WorldWise - Add Map

--- 09-world-wise/src/components/Form.jsx
+++ 09-world-wise/src/components/Form.jsx
@@ -1,7 +1,6 @@
 // "https://api.bigdatacloud.net/data/reverse-geocode-client?latitude=0&longitude=0"
 
 import { useState } from "react";
-import { useNavigate } from "react-router-dom";
 
 import styles from "./Form.module.css";
 import Button from "./Button";
@@ -15,9 +14,8 @@ export function convertToEmoji(countryCode) {
 }
 
 function Form() {
-  const navigate = useNavigate();
   const [cityName, setCityName] = useState("");
-  const [country, setCountry] = useState("");
+  // const [country, setCountry] = useState("");
   const [date, setDate] = useState(new Date());
   const [notes, setNotes] = useState("");
 
@@ -53,15 +51,7 @@ function Form() {
 
       <div className={styles.buttons}>
         <Button type="primary">Add</Button>
-        <Button
-          type="back"
-          onClick={(e) => {
-            e.preventDefault();
-            navigate(-1);
-          }}
-        >
-          &larr; Back
-        </Button>
+        <BackButton></BackButton>
       </div>
     </form>
   );
--- 09-world-wise/src/components/Map.jsx
+++ 09-world-wise/src/components/Map.jsx
@@ -1,17 +1,34 @@
 import { useNavigate, useSearchParams } from "react-router-dom";
+import { MapContainer, TileLayer, Marker, Popup } from "react-leaflet";
 import styles from "./Map.module.css";
+import { useState } from "react";
 function Map() {
-  const [searchParams, setSearchParams] = useSearchParams();
   const navigate = useNavigate();
+  // const [searchParams, setSearchParams] = useSearchParams();
+  const [mapPostion, setMapPosition] = useState([45, 40]);
 
-  const lat = searchParams.get("lat");
-  const lng = searchParams.get("lng");
+  // const lat = searchParams.get("lat");
+  // const lng = searchParams.get("lng");
 
   return (
-    <div
-      className={styles.mapContainer}
-      onClick={() => navigate("form")}
-    >{`Postion: ${lat} ${lng}`}</div>
+    <div className={styles.mapContainer} onClick={() => navigate("form")}>
+      <MapContainer
+        className={styles.map}
+        center={mapPostion}
+        zoom={13}
+        scrollWheelZoom={true}
+      >
+        <TileLayer
+          attribution='&copy; <a href="https://www.openstreetmap.fr/hot/copyright">OpenStreetMap</a> contributors'
+          url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+        />
+        <Marker position={mapPostion}>
+          <Popup>
+            A pretty CSS3 popup. <br /> Easily customizable.
+          </Popup>
+        </Marker>
+      </MapContainer>
+    </div>
   );
 }
 
